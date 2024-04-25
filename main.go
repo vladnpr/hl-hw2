b3bdeae8c14da5434f4cd38e699c015ed6c92480
Vladyslav <vladnpr@gmail.com>
2024-04-25 13:46:11 +0300

    add method 'CompleateTask'

--- main.go
+++ main.go
@@ -9,41 +9,53 @@ type Task struct {
 
 type TasksList map[int]*Task
 
-func (tl *TasksList) add(task *Task) {
-	var key int
-
-	for {
-		_, ok := (*tl)[key]
-		if !ok {
-			(*tl)[key] = task
-			fmt.Println("Added task:", task.Title)
-			break
-		} else {
-			key++
-			continue
-		}
+func (tl TasksList) Add(task *Task) {
+	key := len(tl)
+	_, ok := tl[key]
+	if !ok {
+		key = len(tl) + 1
 	}
+
+	tl[key] = task
+	fmt.Println("Added task:", task.Title)
 }
 
-func (tl *TasksList) delete(key int) {
-	_, ok := (*tl)[key]
+func (tl TasksList) Delete(key int) {
+	_, ok := tl[key]
 	if !ok {
 		fmt.Printf("TaskList does not contain a Task with the specified id: %d n\n", key)
-	} else {
-		delete(*tl, key)
-		fmt.Printf("Task %d has been deleted\n", key)
+		return
 	}
+
+	delete(tl, key)
+	fmt.Printf("Task %d has been deleted\n", key)
+
 }
 
-func (tl *TasksList) print() {
-	if len(*tl) == 0 {
+func (tl TasksList) Print() {
+	if len(tl) == 0 {
 		fmt.Println("TaskList is empty")
-	} else {
-		fmt.Println("TaskList: ")
-		for key, value := range *tl {
-			fmt.Printf("Task %d: %s\n", key, value.Title)
-		}
+		return
 	}
+
+	fmt.Println("TaskList: ")
+	for key, value := range tl {
+		fmt.Printf("Task %d: %s\n", key, value.Title)
+	}
+
+}
+
+func (tl TasksList) CompleteTask(taskID int) {
+	_, ok := tl[taskID]
+
+	if !ok {
+		fmt.Printf("TaskList does not contain a Task with the specified id: %d n\n", key)
+		return
+	}
+
+	task := tl[taskID]
+	task.Completed = true
+	fmt.Printf("Task %d marked as ompleted\n", taskID)
 }
 
 func main() {
@@ -51,10 +63,10 @@ func main() {
 	task0 := Task{Title: "Read a book", Completed: false}
 	task1 := Task{Title: "Play some games", Completed: true}
 
-	tasks.add(&task0)
-	tasks.print()
-	tasks.add(&task1)
-	tasks.print()
-	tasks.delete(0)
-	tasks.print()
+	tasks.Add(&task0)
+	tasks.Print()
+	tasks.Add(&task1)
+	tasks.Print()
+	tasks.Delete(0)
+	tasks.Print()
 }
